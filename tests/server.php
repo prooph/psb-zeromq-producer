42733dd302552355ca835db8c093174083de88a0
Bradley Weston <brad@legalweb.org.uk>
2015-09-27 16:36:08 +0100

    Lets just make sure the server does start.

--- tests/server.php
+++ tests/server.php
@@ -8,6 +8,8 @@ $context = new ZMQContext;
 $socket = new ZMQSocket($context, ZMQ::SOCKET_PULL);
 $socket->bind("tcp://127.0.0.1:5555");
 
+echo "ZMQ Stub Server Started.";
+
 while ($message = $socket->recv()) {
     file_put_contents(__DIR__ . '/zmq-out.log', $message . PHP_EOL, FILE_APPEND);
 }
